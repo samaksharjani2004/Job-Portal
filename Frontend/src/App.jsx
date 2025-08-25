6f99474729ebfe07dd321b43f6d8e69844f27dc5
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-08-26 00:31:27 +0530

    first commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
 import { RouterProvider } from 'react-router'
 import Home from './components/Home'
-
+import Jobs from './components/Jobs'
 
 const appRouter = createBrowserRouter([
   {
@@ -20,6 +20,11 @@ const appRouter = createBrowserRouter([
     path: '/signup',
     element: <Signup />
   },
+  {
+    path: '/jobs',
+    element: <Jobs />
+  },
+
 ])
 
 function App() {
