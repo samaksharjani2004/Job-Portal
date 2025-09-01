b83f227ff1337a2d88e692e81a3c494f0e01a89c
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-09-02 00:16:05 +0530

    first commit

--- Frontend/src/components/Profile.jsx
+++ Frontend/src/components/Profile.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './shared/Navbar'
 import { Avatar, AvatarImage } from './ui/avatar'
 import { Button } from './ui/button'
@@ -6,13 +6,16 @@ import { Contact, Mail, Pen } from 'lucide-react'
 import { Badge } from './ui/badge'
 import { Label } from './ui/label'
 import AppliedJobTable from './AppliedJobTable'
+import UpdateProfileDialog from './UpdateProfileDialog'
 
 
 const skills = ["Html", "Css", "Javascript", "Reactjs"]
 const isResume = true;
 
 
+
 const Profile = () => {
+    const [open, setOpen] = useState(false);
     return (
         <div>
             <Navbar />
@@ -29,7 +32,7 @@ const Profile = () => {
                             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
                         </div>
                     </div>
-                    <Button className="text-right" variant="outline">
+                    <Button onClick={()=> setOpen(true)} className="text-right" variant="outline">
                         <Pen />
                     </Button>
                 </div>
@@ -72,6 +75,7 @@ const Profile = () => {
                 {/* Application Table */}
                 <AppliedJobTable />
             </div>
+             <UpdateProfileDialog open={open} setOpen={setOpen}/>
         </div>
     )
 }
