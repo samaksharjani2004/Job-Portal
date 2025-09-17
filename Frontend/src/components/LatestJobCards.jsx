9e19647031ab85b262ac53e769bb0f12b07036d8
SAMAKSH ARJANI <iamsimran2004@gmail.com>
2025-09-17 20:21:15 +0530

    first commit

--- Frontend/src/components/LatestJobCards.jsx
+++ Frontend/src/components/LatestJobCards.jsx
@@ -1,29 +1,27 @@
 import React from 'react'
 import { Badge } from './ui/badge'
+import { useNavigate } from 'react-router-dom'
 
-const LatestJobCards = () => {
-  return (
-    <div className='p-5 rounded-md shadow-xl bg-white border border-gray-100 cursor-pointer'>
-      <div>
-        <h3 className='font-medium text-lg'>Company Name</h3>
-        <p className='text-sm text-gray-500'>India</p>
-      </div>
+const LatestJobCards = ({job}) => {
+    const navigate = useNavigate();
+    return (
+        <div onClick={()=> navigate(`/description/${job._id}`)} className='p-5 rounded-md shadow-xl bg-white border border-gray-100 cursor-pointer'>
+            <div>
+                <h2 className='font-medium text-lg'>{job?.company?.name}</h2>
+                <p className='text-sm text-gray-500'>India</p>
+            </div>
+            <div>
+                <h2 className='font-bold text-lg my-2'>{job?.title}</h2>
+                <p className='text-sm text-gray-600'>{job?.description}</p>
+            </div>
+            <div className='flex items-center gap-2 mt-4'>
+                <Badge className={'text-blue-700 font-bold'} variant="ghost">{job?.position} Positions</Badge>
+                <Badge className={'text-[#F83002] font-bold'} variant="ghost">{job?.jobType}</Badge>
+                <Badge className={'text-[#7209b7] font-bold'} variant="ghost">{job?.salary}LPA</Badge>
+            </div>
 
-      <div>
-        <h3 className='font-bold text-lg my-2'>Job Title</h3>
-        <p className='text-sm text-gray-600'>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </p>
-      </div>
-
-      <div className='flex items-center gap-2 mt-4'>
-        <Badge className='text-blue-700 font-bold' variant="ghost">12 Positions</Badge>
-        <Badge className='text-[#F83002] font-bold' variant="ghost">Part Time</Badge>
-        <Badge className='text-[#7209b7] font-bold' variant="ghost">24LPA</Badge>
-      </div>
-    </div>
-
-  )
+        </div>
+    )
 }
 
 export default LatestJobCards
--- Frontend/src/components/shared/navbar.jsx
+++ Frontend/src/components/shared/navbar.jsx
@@ -1,14 +1,35 @@
 import React from 'react'
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
-import { Button } from '../ui/button' // ✅ Add this import
+import { Button } from '../ui/button'
 import { Avatar, AvatarImage } from '../ui/avatar'
 import { LogOut, User2 } from 'lucide-react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
+import axios from 'axios'
+import { USER_API_END_POINT } from '@/utils/constant'
+import { setUser } from '@/redux/authSlice'
+import { toast } from 'sonner'
+
 
 
 const Navbar = () => {
         const { user } = useSelector(store => store.auth);
+        const dispatch = useDispatch();
+        const navigate = useNavigate();
+
+         const logoutHandler = async () => {
+        try {
+            const res = await axios.get(`${USER_API_END_POINT}/logout`, { withCredentials: true });
+            if (res.data.success) {
+                dispatch(setUser(null));
+                navigate("/");
+                toast.success(res.data.message);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.response.data.message);
+        }
+    }
 
     return (
         <div className='bg-white'>
@@ -46,8 +67,8 @@ const Navbar = () => {
                                                 <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
                                             </Avatar>
                                             <div>
-                                                <h4 className='font-medium'>Patel MernStack</h4>
-                                                <p className='text-sm text-muted-foreground'> Lorem ipsum dolor sit amet.</p>
+                                                <h4 className='font-medium'>{user?.fullname}</h4>
+                                                <p className='text-sm text-muted-foreground'>{user?.profile?.bio}</p>
                                             </div>
                                         </div>
                                         <div className='flex flex-col my-2 text-gray-600'>
@@ -59,7 +80,7 @@ const Navbar = () => {
                                             </div>
                                             <div className='flex w-fit items-center gap-2 cursor-pointer'>
                                                 <LogOut />
-                                                <Button variant="link">Logout</Button>
+                                                <Button onClick={logoutHandler} variant="link">Logout</Button>
                                             </div>
                                         </div>
                                     </div>
